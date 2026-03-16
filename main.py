1d418d5776455d295954633becbdec8eaa35305f
Ruslan <rusiksadun@gmail.com>
2026-03-16 21:09:30 +0300

    изменил классы

--- main.py
+++ main.py
@@ -1,55 +1,22 @@
-# class GameCharacter:
-#     def __init__(self, name, damage, health):
-#         self.name = name
-#         self.damage = damage
-#         self.health = health
+class GameCharacter:
+    def __init__(self, name, damage, health):
+        self.name = name
+        self.damage = damage
+        self.health = health
     
-#     def attack(self):
-#         print(f"{self.name} наносит {self.damage} урона")
+    def attack(self):
+        print(f"{self.name} наносит {self.damage} урона")
     
-#     def take_damage(self, amount):
-#         self.health -= amount
-#         if self.health < 0:
-#             self.health = 0
-#             print(f"Убит")
-#             return
-#         print(f"{self.name} получил {amount} урона. Осталось здоровья {self.health}")
-
-
-# user1 = GameCharacter(name="Gleb", damage=17, health=80)
-# user1.attack()
-# user1.take_damage(23)
-# user1.take_damage(12)
-
-
-class Cart:
-    def __init__(self, owner,):
-        self.owner = owner
-        self.items = []
-    
-    def add_item(self, name, price):
-        self.items.append(dict(name=name, price=price))
-    
-    def get_total(self):
-        total = sum(el['price'] for el in self.items)
-        return total
-
-    def show_items(self):
-        if not self.items:
-            print("Корзина пуста")
+    def take_damage(self, amount):
+        self.health -= amount
+        if self.health < 0:
+            self.health = 0
+            print(f"Убит")
             return
-        for el in self.items:
-            print(f"{el['name']} - {el['price']}")
+        print(f"{self.name} получил {amount} урона. Осталось здоровья {self.health}")
 
 
-user1 = Cart(owner="Gleb")
-
-user1.add_item(name="iPhone 17", price=80000)
-user1.add_item(name="iPhone 15", price=50000)
-user1.add_item(name="Дерьмо", price=100)
-print()
-user1.get_total()
-print()
-user1.show_items()
-print()
-print(user1.items)
+user1 = GameCharacter(name="Gleb", damage=17, health=80)
+user1.attack()
+user1.take_damage(23)
+user1.take_damage(12)
